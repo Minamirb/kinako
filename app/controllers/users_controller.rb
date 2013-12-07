e4860a1ebace6c508157fbfa463414f504c37d50
Takahiro <studio.quad9@gmail.com>
2013-12-07 16:22:48 +0900

    modify "edit user"
    ユーザー情報の編集をヘッダーのボタンに集約する。
    ユーザー情報の新規作成、削除の機能を削除しました。

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,22 +1,11 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
-
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
-  end
+  before_action :set_user, only: [:show, :edit, :update]
 
   # GET /users/1
   # GET /users/1.json
   def show
   end
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
+  
   # GET /users/1/edit
   def edit
   end
@@ -32,16 +21,6 @@ class UsersController < ApplicationController
         format.html { render action: 'edit' }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
-    end
-  end
-
-  # DELETE /users/1
-  # DELETE /users/1.json
-  def destroy
-    @user.destroy
-    respond_to do |format|
-      format.html { redirect_to users_url }
-      format.json { head :no_content }
     end
   end
 
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,22 +5,6 @@ class UsersControllerTest < ActionController::TestCase
     @user = users(:one)
   end
 
-  test "should get index" do
-    get :index
-    assert_response :success
-    assert_not_nil assigns(:users)
-  end
-
-  test "should get new" do
-    get :new
-    assert_response :success
-  end
-
-  test "should show user" do
-    get :show, id: @user
-    assert_response :success
-  end
-
   test "should get edit" do
     get :edit, id: @user
     assert_response :success
@@ -29,13 +13,5 @@ class UsersControllerTest < ActionController::TestCase
   test "should update user" do
     patch :update, id: @user, user: { email: @user.email, kidlemail: @user.kidlemail, name: @user.name }
     assert_redirected_to user_path(assigns(:user))
-  end
-
-  test "should destroy user" do
-    assert_difference('User.count', -1) do
-      delete :destroy, id: @user
-    end
-
-    assert_redirected_to users_path
   end
 end
